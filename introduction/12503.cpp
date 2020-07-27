c8d75ce5663c9025b6d122ccd360cc658d46abad
AG <amirgh1380@gmail.com>
2020-07-27 23:35:30 +0430

    added 12503

---
+++ introduction/12503.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int tt;
+    int number_of_instructions;
+    scanf("%d",&tt);
+    while(tt--)
+    {
+        int p = 0;
+        scanf("%d",&number_of_instructions);
+        for (int j = 0; j < number_of_instructions; j++)
+        {
+            int array[100];
+            char instruction[10];
+
+            scanf("%s",instruction);
+
+            if (strcmp(instruction,"LEFT") == 0)
+            {
+                p--;
+                array[j] = -1;
+            }
+            else if (strcmp(instruction,"RIGHT") == 0)
+            {
+                p++;
+                array[j] = 1;
+            }
+            else
+            {
+                int number;
+                scanf("%s %d",instruction,&number);
+                p += array[number-1];
+                array[j] = array[number-1];
+            }
+            
+        }
+        printf("%d\n",p);
+        
+    }
+
+    return 0;
+}
