baeb475d4327b78ef4cc5d58dae0567ec3f2a6b2
AmirAG80 <amirgh1380@gmail.com>
2020-07-30 00:36:14 +0330

    added 573

---
+++ introduction/Medium/573.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int height,climb,slide,percent,i;
+    double minus,result,day;
+    while (scanf("%d %d %d %d",&height,&climb,&slide,&percent) && height && climb && slide && percent){
+        minus = 1.0*climb * percent / 100.0;
+        result = 0;
+        day = 0;
+        i = 0;
+        while(1){
+            day = 1.0*(climb-(1.0*i*minus));
+            result += day;
+            i++;
+            if (result > height){
+                printf("success on day %d\n",i);
+                break;
+            }
+            result -= 1.0*slide;
+            if (result < 0){
+                printf("failure on day %d\n",i);
+                break;
+            }
+        }
+    }
+    return 0;
+}
