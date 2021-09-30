a64412387db033718d1125edce287e7886bf15e2
Amirhosein <amirhosein@localhost.localdomain>
2021-09-30 17:37:00 +0330

    added 452.cpp

--- UVA online/455.cpp
+++ UVA online/455.cpp
@@ -1,46 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> prefix(string s) {
+int prefix(string s) {
     int n = (int)s.length();
     vector<int> pi(n);
-    for (int i = 1; i < n; i++) {
-        int j = pi[i-1];
-        while (j > 0 && s[i] != s[j])
-            j = pi[j-1];
-        if (s[i] == s[j])
-            j++;
+    int j = -1;
+    pi[0] = -1;
+
+    for(int i = 1 ; i < n; i++){
+        while(j >= 0 && s[j+1] != s[i])
+            j = pi[j];
+        if(s[j+1] == s[i])
+            ++j;
         pi[i] = j;
     }
-    return pi;
+    
+    return j;
 }
 
 int main(){
 
-    int index = 0;
     string test;
 
     int tt;
     cin >> tt;
     while(tt--){
         cin >> test;
-        vector<int> res = prefix(test);
-        int i;
-        for(i = 0 ; i < res.size(); i++){
-            // if(res[i] != 0){
-            //     index = i;
-            //     break;
-            // }
-            cout << res[i] << " " << test[i] << endl;
-        }
+        int t = prefix(test);
+        int l = test.length();
 
-        cout << index;
-        if(tt != 0)
-            cout << endl << endl;
+        if(l%(l-t-1))
+            cout << l << endl;
+        else
+            cout << l-t-1 << endl;
+
+        if(tt)
+            cout << endl;
     }
     
-    
-    
-
     return 0;
 }
