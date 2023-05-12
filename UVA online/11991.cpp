e91f95d6fc57508469475d68386a6299c8f2b816
amirhosein <amirhosein@cyberdesign.se>
2023-05-12 10:56:26 +0330

    UVA Online accepted 11991

--- UVA online/11991.cpp
+++ UVA online/11991.cpp
@@ -2,37 +2,40 @@
 
 using namespace std;
 
-int main(){
-    int n,m,number,k,v,count,index,flag;
-    vector<int> vec;
-    while (cin >> n >> m){
-        for(int i = 0 ; i < n ; i++){
-            cin >> number;
-            vec.push_back(number);
+vector<vector<int>> graph;
+
+
+int main() {
+
+    int n, m;
+
+    while(true) {
+
+        int res = scanf("%d %d", &n, &m);
+        if(res != 2)
+            break;
+
+        graph.clear();
+        graph = vector<vector<int>>(1000010, vector<int>());
+
+        int num;
+        for(int i = 0; i < n; i++) {
+            cin >> num;
+            graph[num].push_back(i+1);
         }
-        for(int i = 0 ; i< m ; i++){
-            count = 0;
-            index = 0;
-            flag = 0;
-            cin >> k >> v;
-            for(auto a: vec){
-                index++;
-                if(a == v){
-                    count++;
-                }
-                if(count == k){
-                    flag = 1;
-                    break;
-                }
-            }
-            if(flag){
-                cout << index << endl;
-            }
-            else{
-                cout << "0" << endl;
+
+        int occur, number;
+        for(int i = 0; i < m; i++) {
+            cin >> occur >> number;
+
+            if(graph[number].size() < occur) {
+                cout << 0 << endl;
+            } else {
+                cout << graph[number][occur-1] << endl;
             }
         }
+
     }
-    
+
     return 0;
 }
